bb142175afdbfaba101923dfb1d7cba552c477d9
misterabvir <to@misterabvir.ru>
2023-09-12 21:42:58 +0300

    Added domain models

---
+++ Domain/ProjectModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain;
+
+public class ProjectModel
+{
+    public Guid Id { get; set; }
+    public string? ProjectName { get; set; }
+    public DateTime CreateDate { get; set; }
+    public DateTime UpdateDate { get; set; }
+}
---
+++ Domain/TaskModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain;
+
+public class TaskModel
+{
+    public Guid Id { get; set; }
+    public string? TaskName { get; set; }
+    public Guid? ProjectId { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime CancelDate { get; set; }
+    public DateTime CreateDate { get; set; }
+    public DateTime UpdateDate { get; set; }
+}
